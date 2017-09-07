bfcaeb3b2e57507779854cf3cdf49d989bc7e44f
RastaPopoulos <rastapopoulos@spip.org>
2017-09-07 23:22:45 +0200

    Initialisation avec ce qui avait été commencé il y a 2 ans.

---
+++ squelettes/upb_squelettes_options.php
@@ -0,0 +1,16 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+// Activer HTML5 depuis le squelette
+$GLOBALS['meta']['version_html_max'] = 'html5';
+
+if (!isset($GLOBALS['z_blocs'])){
+	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+}
+
+define('_ZENGARDEN_FILTRE_THEMES','upb');
+define('_ALBUMS_INSERT_HEAD_CSS',false);
+define('_INTRODUCTION_SUITE', '…');
+
